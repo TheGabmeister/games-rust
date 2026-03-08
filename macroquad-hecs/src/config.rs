6c6d03b0aa3bb3a8852f132286905fab5d8feeb1
Gabriel Enriquez <thegabmeister@protonmail.com>
2026-03-08 13:28:09 +0800

    Reset project

--- macroquad-hecs/src/config.rs
+++
@@ -1,8 +0,0 @@
-pub const PLAYER_SPEED: f32 = 260.0;
-
-pub const WORLD_WIDTH: f32 = 960.0;
-pub const WORLD_HEIGHT: f32 = 540.0;
-
-pub const FIXED_DT: f32 = 1.0 / 60.0;
-pub const MAX_FRAME_DT: f32 = 0.25;
-pub const MAX_SIM_STEPS_PER_FRAME: u32 = 8;
