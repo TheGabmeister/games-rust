006fd15d907a613e8091358e2b6563f13eb645ad
Gabriel Enriquez <thegabmeister@protonmail.com>
2026-03-08 08:48:09 +0800

    Update asteroids

--- asteroids/src/main.rs
+++ asteroids/src/main.rs
@@ -1,5 +1,6 @@
 use macroquad::prelude::*;
 
+mod game;
 use game::Game;
 
 fn window_conf() -> Conf {
@@ -17,7 +18,7 @@ async fn main() {
 
     loop {
         game.update();
-        if game.quit {
+        if game.should_quit {
             break;
         }
         game.draw();
