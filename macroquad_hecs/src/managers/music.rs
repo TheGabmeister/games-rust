36b0558721594d3a0b51bd392ec0d64af383d40f
Gabriel Enriquez <thegabmeister@protonmail.com>
2026-03-23 13:14:11 +0800

    Fix music

--- macroquad_hecs/src/managers/music.rs
+++ macroquad_hecs/src/managers/music.rs
@@ -1,4 +1,4 @@
-use macroquad::audio::{PlaySoundParams, Sound, play_sound};
+use macroquad::audio::{PlaySoundParams, Sound, play_sound, stop_sound};
 use std::collections::HashMap;
 
 use crate::events::MusicId;
@@ -7,16 +7,25 @@ use super::Assets;
 
 pub struct MusicManager {
     tracks: HashMap<MusicId, Sound>,
+    current: Option<MusicId>,
 }
 
 impl MusicManager {
     pub fn new(assets: &Assets) -> Self {
         Self {
             tracks: assets.music_bank().clone(),
+            current: None,
         }
     }
 
     pub fn play_music(&mut self, id: MusicId) {
+        // Stop the currently playing track first.
+        if let Some(cur) = self.current {
+            if let Some(sound) = self.tracks.get(&cur) {
+                stop_sound(sound);
+            }
+        }
+
         if let Some(sound) = self.tracks.get(&id) {
             play_sound(
                 sound,
@@ -25,6 +34,7 @@ impl MusicManager {
                     volume: 1.0,
                 },
             );
+            self.current = Some(id);
         }
     }
 }
